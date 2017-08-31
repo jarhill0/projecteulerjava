9fb615b0bccc7f4ad13e053748125a5419ebaa02
jarhill0 <jarhill0@gmail.com>
2017-08-30 20:18:44 -0700

    ✨ Solve Problem 10

---
+++ src/Euler010.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Euler010 {
+    private static ArrayList<Integer> sieve(Integer exclusiveMax) {
+        ArrayList<Boolean> bools = new ArrayList<>();
+        ArrayList<Integer> primes = new ArrayList<>();
+        bools.add(false);
+        bools.add(false);
+        for (int i = 2; i < exclusiveMax; i++) {
+            bools.add(true);
+        }
+
+        for (int n = 2; n < exclusiveMax; n++) {
+            if (bools.get(n)) {
+                for (int i = 2 * n; i < exclusiveMax; i += n) {
+                    bools.set(i, false);
+                }
+            }
+        }
+
+        for (int num = 2; num < exclusiveMax; num++) {
+            if (bools.get(num)) {
+                primes.add(num);
+            }
+        }
+
+        return primes;
+    }
+
+    private static Long calculate() {
+        ArrayList<Integer> primes = sieve(2000000);
+        Long sum = 0L;
+        for (int num : primes) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate());
+    }
+}
