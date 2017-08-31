89bdce8e4dfece2ae30565374190abe2cb2ecd0c
jarhill0 <jarhill0@gmail.com>
2017-08-31 13:52:18 -0700

    🐛 Solve Problem 15, for real

--- src/Euler015.java
+++ src/Euler015.java
@@ -1,16 +1,20 @@
+import java.math.BigInteger;
+
 public class Euler015 {
-    private static Long factorial(Integer n) {
-        Long prod = 1L;
-        for (int i = n; i <1; i--) {
-            prod *= i;
+    private static BigInteger factorial(Integer n) {
+        BigInteger prod = BigInteger.valueOf(1);
+        for (int i = n; i > 1; i--) {
+            prod = prod.multiply(BigInteger.valueOf(i));
         }
         return prod;
     }
 
     private static Long calculate() {
-        Long numerator = factorial(20);
-        Long denominator = 2 * factorial(10);
-        return numerator / denominator;
+        // Very memory inefficient. It would make a lot more sense to multiply in and divide out so that we can use longs only.
+        BigInteger numerator = factorial(40);
+        BigInteger denominator_sqrt = factorial(20);
+        BigInteger denominator = denominator_sqrt.multiply(denominator_sqrt);
+        return numerator.divide(denominator).longValue();
     }
 
     public static void main(String[] args) {
